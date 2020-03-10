30b8d97d42921cd28e0d115d17c9e6fd38840517
Julio <jcmcf2012@gmail.com>
2020-03-10 20:06:29 -0300

    Initial Commits

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jcmcf\Desktop\Projeto Podcast\django-react\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
